b92d8a6fa18bfe5cd8cdac5faa5ccf00fc4db85a
Iman Mousaei <imanmousaei1379@gmail.com>
2021-05-22 18:45:29 +0430

    CNN 99.16 accuracy

--- main.m
+++ main.m
@@ -15,7 +15,7 @@ load('mnist.mat');
 
 %% vars
 alpha = 0.1; % learning rate
-layers = [10]; % number of nodes in each hidden layer
+layers = [512,512]; % number of nodes in each hidden layer
 epochs = 20;
 px = 28; % each image is 28*28
 Nlayers = numel(layers);
@@ -26,6 +26,9 @@ trainY = double(trainY);
 testY = double(testY);
 trainN = size(trainX,2);
 testN = size(testX,2);
+
+[XTrain,YTrain] = digitTrain4DArrayData;
+[XTest,YTest] = digitTest4DArrayData;
 
 trainHists = zeros(32,trainN);
 testHists = zeros(32,testN);
@@ -60,13 +63,14 @@ end
 
 
 
-%% MLP
+%% MLP without softmax
 
 net = feedforwardnet(layers);
 net.layers{1}.transferFcn = 'logsig';
 % net.layers{2}.transferFcn = 'radbas';
+net.layers{2}.transferFcn = 'softmax';
 % net.layers{3}.transferFcn = 'purelin';
-net = train(net,trainHists,trainY);
+net = train(net,trainHists,trainY); % todo: mini batch size
 yHatMLP = net(testHists);
 % yHatMLP = predict(net,testHists);
 view(net)
@@ -75,8 +79,30 @@ yy = discretize(testY, 0:9, 'categorical');
 yh = discretize(yHatMLP, 0:9, 'categorical');
 
 figure
-plotconfusion(yy,yh);
+plotconfusion(categorical(round(testY)),categorical(round(yHatMLP)));
 
+
+%% CNN
+NNLayers = [...
+    imageInputLayer([28,28,1])
+    convolution2dLayer(3,64)
+    reluLayer
+    maxPooling2dLayer(4)
+    convolution2dLayer(3,32)
+    reluLayer
+    maxPooling2dLayer(4)
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
+   
+opts = trainingOptions('sgdm', ...
+    'MaxEpochs',10, ...
+    'ValidationData',{XTest,YTest}, ...
+    'ValidationFrequency',50, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+net = trainNetwork(XTrain,YTrain,NNLayers,opts);
 
 %% naive bayes
 nb = fitcnb(X(idxtt,:),Y(idxtt),'Weights',W(idxtt));
@@ -90,4 +116,5 @@ knn = fitcknn(X,Y,'NumNeighbors',3);
 %% SAE ( pretrained MLP with auto encoder )
 
 
+%% SVM
 
